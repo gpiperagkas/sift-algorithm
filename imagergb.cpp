698b74ae09f37dbcfbc8712ddfc7f36ce860c2d9
Grigorios Piperagkas <103881773+gpiperagkas@users.noreply.github.com>
2023-03-19 17:18:08 +0200

    Update imagergb.cpp

--- imagergb.cpp
+++ imagergb.cpp
@@ -2,7 +2,7 @@
 //  imagergb.cpp
 //  colorDescriptor
 //
-//  Copyright 2022 Grigorios Piperagkas. 
+//  Copyright 2022 Grigorios Piperagkas.  All rights reserved.
 //  3-clause BSD License
 //
 //
